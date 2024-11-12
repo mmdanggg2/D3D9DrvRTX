090163601ac5cc81371c1f435e1186cf5c5f859c
James Horsley <jbhorsley@gmail.com>
2024-11-12 00:44:08 +0000

    Add GitHub build action

--- scripts/BuildAll.py
+++ scripts/BuildAll.py
@@ -1,3 +1,4 @@
+import argparse
 import re
 import os
 from pathlib import Path
@@ -19,33 +20,44 @@ def get_version():
 		# Skip the BOM if present
 		if rc_file_content.startswith('\ufeff'):
 			rc_file_content = rc_file_content[1:]
-		version_match = re.search(r"FILEVERSION\s+(\d+),(\d+),(\d+)", rc_file_content)
-		if version_match:
-			version_parts = version_match.groups()
+		version_major_match = re.search(r"#define\s+VERSION_MAJOR\s+(\d+)", rc_file_content)
+		version_minor_match = re.search(r"#define\s+VERSION_MINOR\s+(\d+)", rc_file_content)
+		version_patch_match = re.search(r"#define\s+VERSION_PATCH\s+(\d+)", rc_file_content)
+		if version_major_match and version_minor_match and version_patch_match:
+			version_parts = [
+				version_major_match.group(1),
+				version_minor_match.group(1),
+				version_patch_match.group(1),
+			]
 			version_number = ".".join(version_parts)
 			print(f"Version number extracted: {colored(version_number, 'green')}")
 			return version_number
 		raise RuntimeError("No version found!")
 
-def switch_game(game_code):
+def switch_game(game_code: str):
 	print(f"Switching to '{colored(game_code, 'cyan')}'")
 	subprocess.run([f"{game_code}.bat"], shell=True, check=True)
 
-def build():
+def build(version: str | None = None):
 	solution_path = VS_PROJECT_PATH / "D3D9Drv.sln"
 	configuration = "Release"
-
+	
 	# Build the Visual Studio solution
 	build_command = [
 		"msbuild",
 		str(solution_path),
 		"-verbosity:minimal",
 		"-target:Rebuild",
+		"-m",
 		f"-p:Configuration={configuration}",
 	]
+	if version:
+		# Eww stinky, just shove this in here since the rc has it anyway.
+		with (VS_PROJECT_PATH / "gamename.h").open("a") as f:
+			f.write(f'\n#define VERSION_EXTRA "{version}"\n')
 	subprocess.run(build_command, shell=True, check=True)
 
-def zip_build(build_name, version_path):
+def zip_build(build_name: str, version_path: Path):
 	# Set the files to include in the ZIP file
 	files_to_zip = [
 		VS_PROJECT_PATH / "install" / "System" / "D3D9DrvRTX.dll",
@@ -65,10 +77,18 @@ def zip_build(build_name, version_path):
 
 	print(f"Zipped at: {zip_file_path}")
 
-version = get_version()
+parser = argparse.ArgumentParser()
+parser.add_argument("--version-extra", required=False)
+parser.add_argument("-o", "--overwrite", action="store_true", required=False)
+args = parser.parse_args()
+
+if args.version_extra:
+	version = args.version_extra
+else:
+	version = get_version()
 
 version_path = VS_PROJECT_PATH / "Releases" / version
-version_path.mkdir(parents=True, exist_ok=False)
+version_path.mkdir(parents=True, exist_ok=args.overwrite)
 
 games_codes = [
 	"UT_469d",
@@ -83,8 +103,7 @@ games_codes = [
 
 for game_code in games_codes:
 	switch_game(game_code)
-	build()
+	build(args.version_extra)
 	build_name = f"D3D9DrvRTX-{game_code}-{version}"
 	zip_build(build_name, version_path)
 	print()
-
--- scripts/ChangeLinks.bat
+++ scripts/ChangeLinks.bat
@@ -4,4 +4,6 @@ cd /d "%~dp0"
 rmdir ..\sdk
 mklink /J ..\sdk ..\sdks\sdk_%1
 rmdir ..\install
+mkdir ..\installs\install_%1
+mkdir ..\installs\install_%1\System
 mklink /J ..\install ..\installs\install_%1
